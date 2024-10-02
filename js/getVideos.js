2166b9630ffdd481089cb1a8a34bee1c55a9114f
Ashimkayal4 <ashimkayal4@gmail.com>
2024-10-03 01:36:07 +0600

    dynamically check verify

--- js/getVideos.js
+++ js/getVideos.js
@@ -13,16 +13,30 @@ const showVideo = (data) => {
     const input = document.createElement("div");
     input.classList = "card card-compact";
     input.innerHTML = `
-    <figure class="h-[200px]">
-    <img src=${video.thumbnail} class="w-full h-full object-cover"/>
+    <figure class="h-[200px] relative">
+     <img src=${video.thumbnail} class="w-full h-full object-cover"/>
+     ${
+        video.others.posted_date.length === 0 ? "": `
+        <span class="absolute bg-black text-white right-2 bottom-2 p-1 rounded">${video.others.posted_date}</span> 
+        
+        `
+     }
+   
     </figure>
     <div class="px-0 py-2 flex gap-2">
-      <div><img class="h-10 w-10 rounded-full object-cover" src=${video.authors[0].profile_picture}/></div>
+      <div><img class="h-10 w-10 rounded-full object-cover" src=${
+        video.authors[0].profile_picture
+      }/></div>
       <div>
         <h2 class="font-bold">${video.title}</h2>
         <div class="flex items-center gap-2">
          <p>${video.authors[0].profile_name}</p>
-         <img class=" w-5 " src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>
+
+         ${
+           video.authors[0].verified === true
+             ? ` <img class=" w-5 " src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/> `
+             : ""
+         }
         </div>
         <p></p>
       </div>
